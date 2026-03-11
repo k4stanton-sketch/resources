04f8c6373dbb6c33828baaa1f1e94f882017e240
k4stanton-sketch <k4stanton@gmail.com>
2026-03-10 21:08:43 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Dr. Kathy's Mental Health Tips";
+export const SITE_DESCRIPTION = "Free resources for patients";
